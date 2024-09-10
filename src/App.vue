44c7c3f062921908745cdec46a90339246617781
DylanBlanco <dylanblanco1198@gmail.com>
2024-09-10 02:28:17 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+// my-component.js
+export default {
+  data() {
+    return {
+      message: 'Template Vite + VUE'
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
